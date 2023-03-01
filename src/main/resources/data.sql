5a756ef6ec12b2a28833dc10069772051e633230
NoizyDuck <NoizyDuck84@yandex.ru>
2023-03-01 21:58:05 +0300

    add-item-requests sql error fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-delete from BOOKING;
-delete from ITEMS;
-delete from USERS;
-delete from ITEM_REQUEST;
+-- delete from BOOKING;
+-- delete from ITEMS;
+-- delete from USERS;
+-- delete from ITEM_REQUEST;
 
 ALTER TABLE COMMENTS ALTER COLUMN ID RESTART WITH 1;
 ALTER TABLE BOOKING ALTER COLUMN booking_id RESTART WITH 1;
